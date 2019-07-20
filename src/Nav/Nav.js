4bcd9018bca482a4c0baad6ac4160d810a114425
Dorah <dorah.bmw@gmail.com>
2019-07-20 14:42:40 -0500

    swapped buttons for link components

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,15 +1,15 @@
-import React from 'react';
+//update Nav.js by swapping buttons for Link components
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Nav(props) {
   return (
-    <nav className='Nav'>
-      <button onClick={() => props.clickPage('list')}>
-        Bookmark List
-      </button>
-      {' '}
-      <button onClick={() => props.clickPage('add')}>
-        Add Bookmark
-      </button>
+    <nav className="Nav">
+      {/* <button onClick={() => props.clickPage("list")}>Bookmark List</button>{" "} */}
+      <Link to={"/"}>Bookmark List</Link>
+      {"  "}
+      {/*  <button onClick={() => props.clickPage("add")}>Add Bookmark</button> */}
+      <Link to={"/add-bookmark"}>Add Bookmark</Link>
     </nav>
   );
 }
